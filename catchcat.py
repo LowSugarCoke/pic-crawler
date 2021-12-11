00bd443a33e35bc719853c1a4c723b42cd7d96dc
Jack Lee <jack20499@gmail.com>
2021-12-11 19:59:26 +0800

    add cat breeds

--- catchcat.py
+++ catchcat.py
@@ -2,8 +2,10 @@ import google_catch_image
 
 
 if __name__ == '__main__':   
-    keyword = 'cat'
-    url = 'https://www.google.com.hk/search?q='+keyword+'&tbm=isch'
 
+    keywordlist=['Abyssinian','American Shorthair','Burmilla','Black cat','Cats in ancient Egypt','Turkish Van','Korat']
     crawler_lib = google_catch_image.GoogleCatchImage()
-    crawler_lib.run(url,keyword)
+
+    for keyword in keywordlist:   
+            url = 'https://www.google.com.hk/search?q='+keyword+'&tbm=isch'
+            crawler_lib.run(url,keyword,headless=True,round=70)
--- google_catch_image.py
+++ google_catch_image.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 @author: Jack Lee
-WebCrawlerLib provides to crawler page by using selenium.
-Example:
+Reference: https://www.cxymm.net/article/qq_34687559/106340929
+
+How to use:
+
+import google_catch_image
+
+crawler_lib = google_catch_image.GoogleCatchImage()   
+keyword = 'cat'
+url = 'https://www.google.com.hk/search?q='+keyword+'&tbm=isch'
+round = 70  #download image numbers
+headless = False #(default is False, don't show by setting True) 
+
+crawler_lib.run(url,keyword,headless=headless,round=round)
 
 """
 from selenium import webdriver
@@ -43,32 +54,30 @@ class GoogleCatchImage(CatchImage):
             self.driver.execute_script(js)
             time.sleep(1)
 
+            #catch "img" label
             img_elements = self.driver.find_elements_by_tag_name('img')
-            #遍历抓到的webElement
+            
             for img_element in img_elements:
                 img_url = img_element.get_attribute('src')
-                # 前几个图片的url太长，不是图片的url，先过滤掉，爬后面的
-                if isinstance(img_url, str):
-                    if len(img_url) <= 200:
-                        #将干扰的goole图标筛去
-                        if 'images' in img_url:
-                            #判断是否已经爬过，因为每次爬取当前窗口，或许会重复
-                            #实际上这里可以修改一下，将列表只存储上一次的url，这样可以节省开销，不过我懒得写了···
-                            if img_url not in img_url_dic:
-                                try:
-                                    img_url_dic.append(img_url)
-                                    #下载并保存图片到当前目录下
-                                    filename = picpath+"/" + str(count) + ".jpg"
-                                    r = requests.get(img_url)
-                                    with open(filename, 'wb') as f:
-                                        f.write(r.content)
-                                    f.close()
-                                    count += 1
-                                    print('this is '+str(count)+'st img')
-                                    #防止反爬机制
-                                    time.sleep(0.2)
-                                except:
-                                    print('failure')
+                # insinstance(img_url,str) -> make sure type is str
+                # len(img_url)<=200 -> url too long is not image
+                # 'images' in img_url -> filter google logo
+                # img_url not in img_url_dic -> make sure image not to download repeatly
+                if isinstance(img_url, str) and len(img_url)<=200 and 'images' in img_url and img_url not in img_url_dic:
+                    #download picture to local
+                    try:
+                        img_url_dic.append(img_url)
+                        filename = picpath+"/" + str(count) + ".jpg"
+                        r = requests.get(img_url)
+                        with open(filename, 'wb') as f:
+                            f.write(r.content)
+                            f.close()
+                        count += 1
+                        print('this is '+str(count)+'st img')
+                        
+                        time.sleep(0.2)
+                    except:
+                        print('failure')
         
     def close(self):
         try:
